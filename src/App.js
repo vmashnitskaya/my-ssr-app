f2a89d9fb2f79bc736edacaacba2f0a9ed9b2873
Volha Mashnitskaya <Volha_Mashnitskaya@epam.com>
2020-09-22 15:14:29 +0300

    add ssr app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Home from './Home';
+
+function App() {
+    return (
+        <Home name="SSR rendering"/>
+    )
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = ({name}) => {
+    return (<h1>Hello, {name}</h1>)
+}
+
+export default Home;
